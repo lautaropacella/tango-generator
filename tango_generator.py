bcbc37b45e9dea05b0ec701829eeea3c46d331ab
lautaropacella <59834503+lautaropacella@users.noreply.github.com>
2021-05-07 20:03:45 +0200

    Update tango_generator.py

--- tango_generator.py
+++ tango_generator.py
@@ -27,7 +27,9 @@ top_p = st.sidebar.slider("",value=0.9, min_value=0.5, max_value=1.0, step=0.1)
 st.title('Tango Cromado')
 st.subheader('Generación automática de letras de tango utilizando la arquitectura del Modelo GPT-2')
 st.markdown('Creado por Lautaro Pacella')
+
 st.image(image2)
+
 inicio = st.text_input('Podes empezar con una palabra o frase tuya o dejar que el modelo genere una letra completamente al azar')
 propio = st.button("Generar")
 
